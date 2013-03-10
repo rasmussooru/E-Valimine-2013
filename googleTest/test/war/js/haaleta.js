b77b04986fce8d0417d334ac4f32d25d0af73e4e
rasmussooru <rasmuss@windowslive.com>
2013-03-10 21:25:08 +0200

    parandused

--- googleTest/test/war/js/haaleta.js
+++ googleTest/test/war/js/haaleta.js
@@ -1,30 +1,26 @@
-$(document).ready(function(){
-$('#showdata').ready(function(){
-$.getJSON("data/test.json",function(data){
-          $.each(data.kandidaat, function(i,kandidaat){
-		if(kandidaat.location == 'Tartumaa') {
-              info = '<div id="kandidaat">' +
-                     	'<div id="piltjatekst">' +
-                  			'<div id="pilt">' +
-                     		 	'<img alt="Andrus Ansip" src="view/images/kandidaadid/andrus.jpg">' +
-              				 '</div>' +
-              				 
-              				 '<div id="andmed">' +
-              				 		'<p>Nimi: ' + kandidaat.firstName + '</p>' +
-              				 		'<p>Asukoht: ' + kandidaat.lastName +'</p>' + 
-              				 		'<p>Erakond: ' + kandidaat.location +'</p>' +
-              				 '</div>' +
-              			'</div>' +
-					    '<div id="nupp">' +
-							'<button class="submit" type="submit">H‰‰leta</button>' +
-			            '</div>' +
-			         '</div>';
-                 $(info).appendTo("#boxisisu");
-}
-else {
-     console.log('foo');
-}
-          });
+$(document).ready(function () {
+    $.getJSON("data/test.json", function (data) {
+        $.each(data.kandidaat, function (i, kandidaat) {
+            if (kandidaat.location == 'Tartumaa') {
+                info = '<div id="kandidaat">' +
+                    '<div id="piltjatekst">' +
+                    '<div id="pilt">' +
+                    '<img alt="Andrus Ansip" src="view/images/kandidaadid/andrus.jpg">' +
+                    '</div>' +
+
+                    '<div id="andmed">' +
+                    '<p>Nimi: ' + kandidaat.firstName + '</p>' +
+                    '<p>Perenimi: ' + kandidaat.lastName + '</p>' +
+                    '<p>Maakond: ' + kandidaat.location + '</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '<div id="nupp">' +
+                    '<button class="submit" type="submit">H‰‰leta</button>' +
+                    '</div>' +
+                    '</div>';
+                $(info).appendTo("#boxisisu");
+            }
         });
-});
+    });
+
 });
