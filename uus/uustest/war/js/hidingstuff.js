0a9f0396020052b30c9f7d099a98495ac1e2b1cb
Olari Koppel <a73520@ut.ee>
2013-04-14 17:31:19 +0300

    Panen esialgu tagasi, millegi pärast ei taha töötada.

--- uus/uustest/war/js/hidingstuff.js
+++ uus/uustest/war/js/hidingstuff.js
@@ -16,10 +16,3 @@ function hideStuff(id) {
 	document.getElementById(id).style.display = 'none';
 }
 
-(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/et_EE/all.js#xfbml=1&appId=527973890582304";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
